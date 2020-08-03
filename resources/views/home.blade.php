782b73353a7cad7709143247c213c34275437911
yuuki1218 <1991sy1218@gmail.com>
2020-08-03 14:58:44 +0900

    routes

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.success')
+@section('title', 'ホーム画面')
+@section('content')
+    <div class="container">
+        <h1>ホーム画面</h1>
+        <p>you are logged in!</p>
+    </div>
+@endsection
